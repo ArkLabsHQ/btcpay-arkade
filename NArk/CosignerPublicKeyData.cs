8f1f7623ee3381376d1efbeb73887608a6753acc
Andrew Camilleri <evilkukka@gmail.com>
2026-01-03 22:41:30 +0100

    feat: Replace old NArk library with NNark submodule
    - Add NNark as git submodule at submodules/NNark
    - Remove old NArk/ and NArk.Tests/ directories
    - Update solution file to reference NNark projects
    - Update plugin csproj to use NNark project references
    - NNark provides: core library, abstractions, swaps, gRPC transport, hosting
    
    Note: Plugin code not yet updated to use new NNark types

--- NArk/CosignerPublicKeyData.cs
+++
@@ -1,9 +0,0 @@
-﻿using NBitcoin.Secp256k1;
-
-namespace NArk;
-
-public class CosignerPublicKeyData
-{
-    public byte Index { get; set; }
-    public ECPubKey Key { get; set; }
-}
--- NArk/Extensions/BytesExtentions.cs
+++
@@ -1,9 +0,0 @@
-namespace NArk.Extensions;
-
-public static class BytesExtensions
-{
-    public static string ToHex(this byte[] value)
-    {
-        return Convert.ToHexString(value).ToLowerInvariant();
-    }
-}
